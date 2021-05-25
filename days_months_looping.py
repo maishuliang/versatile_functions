11bcfbd0aceccaeac6027d5d4967ba7f1748b0c7
maishuliang <46385013+maishuliang@users.noreply.github.com>
2021-05-25 15:01:04 -0700

    add the timer function

--- days_months_looping.py
+++ days_months_looping.py
@@ -9,3 +9,11 @@ def iter_by_weeks(year, start_week, end_week):
 
         s_date, e_date = c_week.monday(), c_week.sunday()
 
+import time
+# this function could be used to time how long a function will take
+def timer(func, func_description='this', **func_parameters):
+    s_time = time.time()
+    output = func(**func_parameters)
+    e_time = time.time()
+    print('spend {time}s for {func_description} function'.format(time=e_time-s_time, func_description=func_description))
+    return output
